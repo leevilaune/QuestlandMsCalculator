912b3efa2edc0291e0dcc88a1765f399575ba5e0
leevilaune <npoeka@gmail.com>
2023-06-27 22:23:31 +0300

    Add point calculation and sorting

--- src/main/java/org/leevilaune/mscalculator/domain/Stage.java
+++ src/main/java/org/leevilaune/mscalculator/domain/Stage.java
@@ -1,16 +1,38 @@
 package org.leevilaune.mscalculator.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Stage {
 
 
     private String name;
     private int id;
-
+    private int points;
+    private Map<String, Integer> monsters;
     public Stage(String name, int id){
         this.name = name;
         this.id = id;
+        this.monsters = new HashMap<>();
     }
 
+    public void addMonster(String name, int count){
+        this.monsters.put(name,count);
+    }
+
+    /***
+     * Check whether stage has specified monster
+     * @param monster - name of monster
+     * @return boolean indicating whether monster was found
+     */
+    public boolean hasMonster(String monster){
+        for(String s : monsters.keySet()){
+            if(s.equalsIgnoreCase(monster)){
+                return true;
+            }
+        }
+        return false;
+    }
     public String getName() {
         return name;
     }
@@ -27,4 +49,29 @@ public class Stage {
         this.id = id;
     }
 
+    public Map<String, Integer> getMonsters() {
+        return monsters;
+    }
+
+    public void setMonsters(Map<String, Integer> monsters) {
+        this.monsters = monsters;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Stage{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", points=" + points +
+                ", monsters=" + monsters +
+                '}';
+    }
 }
--- src/main/java/org/leevilaune/mscalculator/ui/CLI.java
+++ src/main/java/org/leevilaune/mscalculator/ui/CLI.java
@@ -1,2 +1,26 @@
-package org.leevilaune.mscalculator.ui;public class CLI {
+package org.leevilaune.mscalculator.ui;
+
+import java.util.Scanner;
+
+public class CLI {
+
+    private Scanner cliReader;
+
+    public CLI(){
+
+        this.cliReader = new Scanner(System.in);
+    }
+    public void start(){
+        while(true){
+            System.out.print("> ");
+            String command = cliReader.next();
+            switch(command){
+                case "exit":
+                    System.out.println("Exited");
+                    break;
+                case "run ms":
+
+            }
+        }
+    }
 }
